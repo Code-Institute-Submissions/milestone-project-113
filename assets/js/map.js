0903f498f415956622bd5d5d7d19ee44e1b94b83
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2020-09-18 15:37:00 +0000

    amended maps.js to change central lat and lng depending on destination and implemented map on sydney and brisbane pages

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,20 +1,30 @@
 function initMap() {
-    let map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 15,
-        center: {
-            lat: -37.8177089,
-            lng: 144.9668995
-        }
+  function getCity() {
+    let cityName = $("h1").html();
+    if (cityName === "Melbourne") {
+      return new google.maps.LatLng(-37.8177089, 144.9668995);
+    } else if (cityName === "Sydney") {
+      return new google.maps.LatLng(-33.8676561, 151.2076709);
+    } else if (cityName === "Brisbane") {
+      return new google.maps.LatLng(-27.4692952, 153.0250476);
+    } else {
+      return false;
+    }
+  }
+
+  let map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 15,
+    center: getCity()
+  });
+
+  let labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+  let locations = [];
+
+  let markers = locations.map(function (location, i) {
+    return new google.maps.Marker({
+      position: location,
+      label: labels[i % labels.length],
     });
-
-    let labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
-    let locations = [];
-
-    let markers = locations.map(function(location, i) {
-        return new google.maps.Marker ({
-            position: location,
-            label: labels [i % labels.length]
-        });
-    });
+  });
 }
