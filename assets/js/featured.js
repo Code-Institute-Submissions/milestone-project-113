d1214a9e52c1bea3def6aa43613333afaa4a0cbe
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2020-09-29 17:40:58 +0000

    changed discover best of australia slideshow to owl carousel

--- assets/js/featured.js
+++ assets/js/featured.js
@@ -1,49 +1,37 @@
-//Most of the below code is from http://jsfiddle.net/bretmorris/ULNa2/7/
-$(document).ready(function () {
-  iniShow();
-  //Click on previous button loads up previous slide.
-  $("button.prev").click(function () {
-    prevSlide($(".slides"));
-  });
-  //Click on next button loads up next slide.
-  $("button.next").click(function () {
-    nextSlide($(".slides"));
-  });
-  //Swipe right loads up next slide. Code adapted from https://api.jquerymobile.com/swiperight/
-  $(".slideshow").on("swipeleft", function () {
-    prevSlide($(".slides"));
-  });
-  //Swipe left loads up next slide. Code adapted from https://api.jquerymobile.com/swipeleft/
-  $(".slideshow").on("swiperight", function () {
-    nextSlide($(".slides"));
-  });
-  //Click on card flips the card to show the content on the back of the card. Code adapted from https://codepen.io/bycreator/pen/RNQmZK
-  $(".card").click(function () {
-    $(this).toggleClass("flipped");
-  });
-});
+//Most of the code below is from https://codepen.io/Anahiiit/pen/wvGPvaQ
 
-//Initialise slide show and display first image
-function iniShow() {
-  $(".slides").each(function () {
-    $(this).children().hide();
-    $(this).children().first().fadeIn(500);
-  });
-}
-
-function prevSlide(slides) {
-  $(slides).children().last().prependTo(slides);
-  showSlide(slides);
-}
-
-function nextSlide(slides) {
-  $(slides).children().first().appendTo(slides);
-  showSlide(slides);
-}
-
-function showSlide(slides) {
-  //hide (reset) all slides
-  slides.children().hide();
-  //fade in next slide
-  slides.children().first().fadeIn(500);
-}
+$(document).ready(function($) {
+    let $owl = $('.owl-carousel');
+      $owl.children().each( function( index ) {
+        $(this).attr( 'data-position', index ); 
+      });
+      
+      $owl.owlCarousel({
+        center: true,
+        nav: true,
+        loop: true,
+        items: 4,
+      margin:10,
+        navText: ["<i class='fas fa-caret-left'></i>","<i class='fas fa-caret-right'></i>"],
+        responsive:{
+          0:{
+              items:2,
+              //Code below from https://owlcarousel2.github.io/OwlCarousel2/docs/api-options.html
+              center: false
+          },
+          768:{
+              items:3,
+              //Code below from https://owlcarousel2.github.io/OwlCarousel2/docs/api-options.html
+              center: false
+          },
+          992:{
+              items:4,
+              //Code below from https://owlcarousel2.github.io/OwlCarousel2/docs/api-options.html
+              center: false
+          }
+       }
+      });
+    $(document).on('click', '.item', function() {
+      $owl.trigger('to.owl.carousel', $(this).data( 'position' ) ); 
+    });
+          });
--- assets/js/init.js
+++
@@ -1,5 +0,0 @@
-//Code below from https://stackoverflow.com/questions/8648596/how-to-use-jquery-mobile-for-its-touch-event-support-only-no-ui-enhancements
-
-$(document).bind("mobileinit", function () {
-  $.extend($.mobile, { autoInitializePage: false });
-});
