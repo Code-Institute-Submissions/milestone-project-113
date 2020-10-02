cd911d2391113ef00ffde0cbf6d4a8f726584ebc
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2020-10-02 13:06:50 +0000

    added jQuery to collapse navbar when contact form link is clicked and prevent the contact us link from going back to the active status when the contact form modal is closed

--- assets/js/email.js
+++ assets/js/email.js
@@ -1,3 +1,17 @@
+$(document).ready(function () {
+  $(".nav-item a:last").click(function () {
+    // Code below from https://getbootstrap.com/docs/4.0/components/collapse/#via-javascript
+    $(".navbar-collapse").collapse("hide");
+    $("#contactUsForm").show();
+  });
+  $(".close").click(function() {
+      // Code below from https://stackoverflow.com/questions/30322918/bootstrap-modal-restores-button-focus-on-close
+    $(".nav-item a:last").one("focus", function() {
+      $(this).blur();
+    });
+  });
+});
+
 function sendMail(contactForm) {
     emailjs.init('user_boozmhp8QWVEdS0lHsScn')
     emailjs.send("gmail", "discover_australia", {
