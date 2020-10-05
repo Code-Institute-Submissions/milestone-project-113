846cbfc2fdab9c0eda01c5db73405aa24739be2a
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2020-10-05 19:23:48 +0000

    Added comments to navbar.js

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,5 +1,6 @@
+// Changes the navbar toggler icon from bars to times when the collapsable navbar is expanded. Code is from https://getbootstrap.com/docs/4.5/components/collapse/#collapseshow
+
 $(document).ready(function () {
-  //Code below from https://getbootstrap.com/docs/4.5/components/collapse/#collapseshow
   $(".navbar-collapse").on("show.bs.collapse", function () {
     $(".navbar-toggler").html(`<i class="fas fa-times"></i>`);
   });
