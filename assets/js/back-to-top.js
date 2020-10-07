f19d4a35a89a0d4867431d165822e152cce26380
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2020-10-07 17:34:16 +0000

    added back to top button

---
+++ assets/js/back-to-top.js
@@ -0,0 +1,16 @@
+// Makes back to top button appear when the user scrolls and they have not reached the footer. Code is from  https://codepen.io/deveb22/pen/QxPmGz and https://stackoverflow.com/questions/34874658/get-the-scroll-distance-from-bottom-to-scroll
+
+$(window).scroll(function () {
+  if ($(window).scrollTop() > 200) {
+    $(".top-of-page").removeClass("d-none");
+  } else {
+    $(".top-of-page").addClass("d-none");
+  }
+});
+
+$(document).ready(function () {
+  $(".top-of-page").on("click", function (e) {
+    e.preventDefault();
+    $("html, body").animate({ scrollTop: 0 }, "200");
+  });
+});
