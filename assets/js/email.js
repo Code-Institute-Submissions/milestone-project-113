e510523f4292c575f46c39890c6b70406c3c17cf
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2020-10-09 21:13:00 +0000

    fixed a bug in email.js preventing map view / list view tabs from being able to be clicked on after the list view tab has been clicked on

--- assets/js/email.js
+++ assets/js/email.js
@@ -1,13 +1,13 @@
 // Collapses navbar when the navbar contact us link is clicked and prevents both contact links from going back to active status when the modal is closed. Code below is from // Code is from // Code below from https://getbootstrap.com/docs/4.0/components/collapse/#via-javascript and https://stackoverflow.com/questions/30322918/bootstrap-modal-restores-button-focus-on-close
 
 $(document).ready(function () {
-  $(".nav-item a:last").click(function () {
+  $(".navbar-nav a:last").click(function () {
     $(".navbar-collapse").collapse("hide");
     $("#contactUsForm").show();
   });
   $(".close").click(function() {
-    $(".nav-item a:last").one("focus", function() {
-      $(this).blur();
+    $(".navbar-nav a:last").one("focus", function() {
+    $(this).blur();
     });
     $(".footer-links a:last").one("focus", function() {
       $(this).blur();
