1f24859ad8d184e2c67909bb8342fb837eb001ba
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2020-10-12 15:55:55 +0000

    Added comments and responses depending on the status returned to email.js

--- assets/js/email.js
+++ assets/js/email.js
@@ -1,38 +1,49 @@
-// Collapses navbar when the navbar contact us link is clicked and prevents both contact links from going back to active status when the modal is closed. Code below is from // Code is from // Code below from https://getbootstrap.com/docs/4.0/components/collapse/#via-javascript and https://stackoverflow.com/questions/30322918/bootstrap-modal-restores-button-focus-on-close
-
 $(document).ready(function () {
+  // Collapses navbar and shows the contact us modal when clicked. Code is from https://getbootstrap.com/docs/4.0/components/collapse/
   $(".navbar-nav a:last").click(function () {
     $(".navbar-collapse").collapse("hide");
     $("#contactUsForm").show();
   });
-  $(".close").click(function() {
-    $(".navbar-nav a:last").one("focus", function() {
-    $(this).blur();
+  // Makes links lose their focus status when the close button is clicked. Code is from https://stackoverflow.com/questions/30322918/bootstrap-modal-restores-button-focus-on-close
+  $(".close").click(function () {
+    $(".navbar-nav a:last").one("focus", function () {
+      $(this).blur();
     });
-    $(".footer-links a:last").one("focus", function() {
+    $(".footer-links a:last").one("focus", function () {
       $(this).blur();
     });
   });
+  // Adds contact form HTML to the element containing the modal-body class before the modal is opened. Code is from https://getbootstrap.com/docs/4.0/components/modal/#via-javascript and https://getbootstrap.com/docs/4.5/components/forms/.
+  $("#contactUsForm").on("show.bs.modal", function () {
+    $(".modal-body").html(
+      `<form onsubmit="return sendMail(this);"><div class="form-group contact-form"><label for="name" class="col-form-label">Name</label><input type="text" name="name" id="name" class="form-control" placeholder="Enter name" required/><label for="emailAddress" class="col-form-label">Email Address</label><input type="text" name="email" id="emailAddress" class="form-control" placeholder="Enter email address" required/><label for="message-text" class="col-form-label">Message:</label><textarea name="message" id="message-text" class="form-control" rows="5" placeholder="Enter message" required></textarea></div><button type="submit" class="btn">Submit</button></form>`
+    );
+  });
 });
 
-// Gets data inputted to the contact form and sends it to emailJS.
+// Gets data inputted to the contact form and sends it to emailJS and run the relevant function depending on whether an error is returned or not.
 
 function sendMail(contactForm) {
-    emailjs.init('user_boozmhp8QWVEdS0lHsScn')
-    emailjs.send("gmail", "discover_australia", {
-        "from_name": contactForm.name.value,
-        "from_email": contactForm.emailAddress.value,
-        "message": contactForm.message.value
+  $(".modal-body button").html("Processing");
+  emailjs.init("user_boozmhp8QWVEdS0lHsScn");
+  emailjs
+    .send("gmail", "discover_australia", {
+      from_name: contactForm.name.value,
+      from_email: contactForm.emailAddress.value,
+      message: contactForm.message.value,
     })
-    .then (
-        function(response) {
-            contactForm.name.value = ''
-            contactForm.emailAddress.value = ''
-            contactForm.message.value = ''
-            console.log("SUCCESS", response)
-        },
-        function(error) {
-            console.log("FAILED", error)
-        });
-    return false;
+    .then(
+      function () {
+        $(".modal-body").html(
+          `<p class="paragraph text-center">Form successfully submitted. <br>A member of the team will be in touch shortly.</p>`
+        );
+      },
+      function (error) {
+        $(".modal-body button").html("Submit");
+        $(".modal-body").prepend(
+          `<p class="paragraph">Error ${error.status}. Please try again.</p>`
+        );
+      }
+    );
+  return false;
 }
