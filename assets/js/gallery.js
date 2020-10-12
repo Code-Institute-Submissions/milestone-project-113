7930d42edf21fe539ff3713afef576cb60a38ae5
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2020-10-12 16:56:09 +0000

    amended email.js to include a function to handle errors and updated comments.

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -11,10 +11,11 @@ let photos = [
   "https://instagram-grabber.p.rapidapi.com/grab/?url=https%253A%252F%252Fwww.instagram.com%252Fp%252FCEuiyrQiim2%252F",
 ];
 
-// Returns the photo associated with each URL in the photos array. Code is from https://rapidapi.com/victor.beck123123/api/instagram-grabber/endpoints 
+// Returns the photo associated with each URL in the photos array.
 
 function fetchPhotos() {
   for (let i = 0; i < photos.length; i++) {
+    // Defines the request parameters and their values. Code is from https://rapidapi.com/victor.beck123123/api/instagram-grabber/endpoints.
     let settings = {
       async: true,
       crossDomain: true,
@@ -25,12 +26,27 @@ function fetchPhotos() {
         "x-rapidapi-key": "3fa6ebdda9mshc8f840afb719d5cp12d66bjsnd08793ce237c",
       },
     };
-    $.getJSON(settings).done(function (response) {
-      console.log(response);
-      $("#gallery").children().eq(i).append(
-        `<img class="gallery-images" src="${response.media[0].source}">`
-      );
-    });
+    // Requests the photo.
+    $.getJSON(settings).then(
+      // Runs this function if a response is received
+      function (response) {
+        $("#gallery")
+          .children()
+          .eq(i)
+          .append(
+            `<img class="gallery-images" src="${response.media[0].source}">`
+          );
+      },
+      // Runs this function if an error is received.
+      function (error) {
+        $("#gallery")
+          .children()
+          .eq(i)
+          .append(
+            `<p class="paragraph gallery-errors text-center">Error ${error.status}. Please refresh the page to try again in 1 minute.</p>`
+          );
+      }
+    );
   }
 }
 
