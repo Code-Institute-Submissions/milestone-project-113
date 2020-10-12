953fc76ce245aa0e903f453b99259d264793fe28
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2020-10-12 21:08:50 +0000

    chnaged type attribute in email address input field to ensure a valid email address is entered

--- assets/js/email.js
+++ assets/js/email.js
@@ -23,7 +23,7 @@ $(document).ready(function () {
   // Adds contact form HTML to the element containing the modal-body class before the modal is opened. Code is from https://getbootstrap.com/docs/4.0/components/modal/#via-javascript and https://getbootstrap.com/docs/4.5/components/forms/.
   $("#contactUsForm").on("show.bs.modal", function () {
     $(".modal-body").html(
-      `<form onsubmit="return sendMail(this);"><div class="form-group contact-form"><label for="name" class="col-form-label">Name</label><input type="text" name="name" id="name" class="form-control" placeholder="Enter name" required/><label for="emailAddress" class="col-form-label">Email Address</label><input type="text" name="email" id="emailAddress" class="form-control" placeholder="Enter email address" required/><label for="message-text" class="col-form-label">Message:</label><textarea name="message" id="message-text" class="form-control" rows="5" placeholder="Enter message" required></textarea></div><button type="submit" class="btn">Submit</button></form>`
+      `<form onsubmit="return sendMail(this);"><div class="form-group contact-form"><label for="name" class="col-form-label">Name</label><input type="text" name="name" id="name" class="form-control" placeholder="Enter name" required/><label for="emailAddress" class="col-form-label">Email Address</label><input type="email" name="email" id="emailAddress" class="form-control" placeholder="Enter email address" required/><label for="message-text" class="col-form-label">Message:</label><textarea name="message" id="message-text" class="form-control" rows="5" placeholder="Enter message" required></textarea></div><button type="submit" class="btn">Submit</button></form>`
     );
   });
 });
