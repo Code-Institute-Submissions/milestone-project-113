2849a97292b6d2a941a4211549f5ff80d11571f1
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2020-10-18 19:25:24 +0000

    amended comments in navbar.js and amended navbar toggler icons so they show text when a screenreader is being used

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,10 +1,10 @@
-// Changes the navbar toggler icon from bars to times when the collapsable navbar is expanded. Code is from https://getbootstrap.com/docs/4.5/components/collapse/#collapseshow
+// Changes the navbar toggler icon from bars to times when the collapsable navbar is expanded. Code is from https://getbootstrap.com/docs/4.5/components/collapse/#collapseshow, https://fontawesome.com/icons/bars?style=solid and https://fontawesome.com/icons/times?style=solid
 
 $(document).ready(function () {
   $(".navbar-collapse").on("show.bs.collapse", function () {
-    $(".navbar-toggler").html(`<i class="fas fa-times"></i>`);
+    $(".navbar-toggler").html(`<i class="fas fa-times aria-hidden="true"></i><span class="sr-only">Bars</span>`);
   });
   $(".navbar-collapse").on("hide.bs.collapse", function () {
-    $(".navbar-toggler").html(`<i class="fas fa-bars"></i>`);
+    $(".navbar-toggler").html(`<i class="fas fa-bars aria-hidden="true"></i><span class="sr-only">Bars</span>`);
   });
 });
